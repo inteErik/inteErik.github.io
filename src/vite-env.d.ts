2be85eefd8fa3d3014bad3b812f25371361b01fd
superBunger <120007693+inteErik@users.noreply.github.com>
2025-04-06 22:18:44 +0200

    Didn't realize you need to build first (Still figuring out how this works :p)

--- src/vite-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="vite/client" />
-export default defineConfig({
-    base: '/inteErik.github.io/', // <- Set to repo name if hosting under a GitHub subpath
-    plugins: [react()],
-})
