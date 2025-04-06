3b59e1d1988993aa21fd506db05d87819f874b6f
superBunger <120007693+inteErik@users.noreply.github.com>
2025-04-06 22:15:46 +0200

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="vite/client" />
+export default defineConfig({
+    base: '/inteErik.github.io/', // <- Set to repo name if hosting under a GitHub subpath
+    plugins: [react()],
+})
